e47d85a7c0c2abe4f4992f784aced2c05a7a9b84
weite122 <949193531@qq.com>
2017-11-14 03:33:46 +0800

    [A]添加所有接口

--- app.js
+++ app.js
@@ -31,18 +31,53 @@ app.use(controller.get('/api_test',function*(){
     this.body = service.get_test_data()
 }))
 
+app.use(controller.get('/ajax/index', function*(){
+	this.set('Cache-Control', 'no-cache')
+	this.body = service.get_index_data()
+}))
 
+app.use(controller.get('/ajax/rank', function*(){
+	this.set('Cache-Control', 'no-cache')
+	this.body = service.get_rank_data()
+}))
+
+app.use(controller.get('/ajax/catedetail', function*(){
+	this.set('Cache-Control', 'no-cache')
+	this.body = service.get_catedetail_data()
+}))
+
+app.use(controller.get('/ajax/category', function*(){
+	this.set('Cache-Control', 'no-cache')
+	this.body = service.get_category_data()
+}))
+
+app.use(controller.get('/ajax/check', function*(){
+	this.set('Cache-Control', 'no-cache')
+	this.body = service.get_check_data()
+}))
+
+app.use(controller.get('/ajax/book', function*(){
+	this.set('Cache-Control', 'no-cache')
+	var querystring = require('querystring')
+	var params = querystring.parse(this.req._parsedUrl.query)
+    var id = params.id
+    if(!id){
+        id = ''
+    }
+	this.body = service.get_book_data(id)
+}))
 
 app.use(controller.get('/ajax/search', function*(){
-	this.set('Cache-Control', 'no-cache');
-	var querystring = require('querystring');
-	var _this = this;
-	var params = querystring.parse(this.req._parsedUrl.query);//将HTTPquerystring转化为obj形式
-    var start = params.start;
-    var end = params.end;
-    var keyword = params.keyword;
-	this.body = yield service.get_search_data(start, end, keyword);//因为异步返回，所以前面想要yield
-}));
+	this.set('Cache-Control', 'no-cache')
+	var querystring = require('querystring')
+	var _this = this
+	var params = querystring.parse(this.req._parsedUrl.query)
+    var start = params.start
+    var end = params.end
+    var keyword = params.keyword
+	this.body = yield service.get_search_data(start, end, keyword)//因为异步返回，所以前面想要yield
+}))
+
 
 
 app.listen(3001)
--- service/webAppService.js
+++ service/webAppService.js
@@ -4,6 +4,38 @@ exports.get_test_data = function(){
     return content
 }
 
+exports.get_index_data = function(){
+    var content = fs.readFileSync('./mock/home.json','utf-8')
+    return content
+}
+
+exports.get_rank_data = function(){
+    var content = fs.readFileSync('./mock/rank.json','utf-8')
+    return content
+}
+
+exports.get_book_data = function(id){
+    if(!id){
+        id = "18218"
+    }
+    var content = fs.readFileSync('./mock/book/'+id+'.json','utf-8')
+    return content
+}
+
+exports.get_catedetail_data = function(){
+    var content = fs.readFileSync('./mock/catedetail.json','utf-8')
+    return content
+}
+
+exports.get_category_data = function(){
+    var content = fs.readFileSync('./mock/category.json','utf-8')
+    return content
+}
+exports.get_check_data = function(){
+    var content = fs.readFileSync('./mock/check.json','utf-8')
+    return content
+}
+
 exports.get_search_data = function(start,end,keyword){
     return function(cb){
         var http = require('http')
