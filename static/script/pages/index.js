051cbaba63ebfd841f8f5c3a6784b32dfffd751f
weite122 <949193531@qq.com>
2017-11-19 23:55:42 +0800

    [A]tab滑动

--- static/script/pages/index.js
+++ static/script/pages/index.js
@@ -8,8 +8,18 @@ $.get('/ajax/index',function(d){
             female:d.items[3].data.data,
             male:d.items[4].data.data,
             free:d.items[5].data.data,
-            topic:d.items[6].data.data
+            topic:d.items[6].data.data,
+            duration:0,
+            position:0,
+            header_position:0,
+            header_duration:0,
+            tab_1_class:'Swipe-tab__on',
+            tab_2_class:''
+        },
+        method:{
+            tabSwitch:function(){
+
+            }
         }
     })
-    console.log(d.items[0].data.data)
 },'json')
