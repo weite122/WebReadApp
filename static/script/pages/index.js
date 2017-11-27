69e39d178c7d546320ffe282f32b032d3369246c
weite122 <949193531@qq.com>
2017-11-27 23:58:59 +0800

    [A]兼容页面优化

--- static/script/pages/index.js
+++ static/script/pages/index.js
@@ -1,36 +1,49 @@
 $.get('/ajax/index',function(d){
-    new Vue({
-        el:'#app',
-        data:{
-            top:d.items[0].data.data,
-            hot:d.items[1].data.data,
-            recommend:d.items[2].data.data,
-            female:d.items[3].data.data,
-            male:d.items[4].data.data,
-            free:d.items[5].data.data,
-            topic:d.items[6].data.data,
-            duration:0,
-            position:0,
-            header_position:0,
-            header_duration:0,
-            tab_1_class:'Swipe-tab__on',
-            tab_2_class:''
-        },
-        methods:{
-            tabSwitch:function(pos){
-               this.duration = 0.5;
-               this.header_duration = 0.5;
-               if(pos === 0){
-                   this.position = 0;
-                   this.header_position = 0;
-                   this.tab_1_class = "Swipe-tab__on";
-                   this.tab_2_class = "";
-               }else{
-                    this.position = -734;
-                    this.header_position = 277;
-                    this.tab_1_class = "Swipe-tab__on";
-                    this.tab_2_class = "";
-               }
+    var windowWidth = $(window).width();
+	if (windowWidth < 320) {
+		windowWidth = 320;
+	}
+	var offset = $($('.Swipe-tab').find('a')[0]).offset();
+	var index_header_tab_width = offset.width;
+	new Vue({
+		el: '#app',
+		data: {
+			screen_width: windowWidth,
+			double_screen_width: windowWidth*2,
+			index_header_tab_width: index_header_tab_width,
+			top: d.items[0].data.data,
+			hot: d.items[1].data.data,
+			recommend: d.items[2].data.data,
+			female: d.items[3].data.data,
+			male: d.items[4].data.data,
+			free: d.items[5].data.data,
+			topic: d.items[6].data.data,
+			duration:0,
+			position: 0,
+			header_position: 0,
+			header_duration: 0,
+			tab_1_class: 'Swiper-tab__on',
+			tab_2_class: '',
+			first_female_index: 0,
+			last_female_index: 5,
+			first_male_index: 0,
+			last_male_index: 5,
+		},
+		methods: {
+			tabSwitch: function(pos) {
+				this.duration = 0.5;
+				this.header_duration = 0.5;
+				if (pos == 0) {
+					this.position = 0;
+					this.header_position = 0;
+					this.tab_1_class = "Swiper-tab__on";
+					this.tab_2_class = "";
+				} else {
+					this.position = (-windowWidth);
+					this.header_position = index_header_tab_width;
+					this.tab_2_class = "Swiper-tab__on";
+					this.tab_1_class = "";
+				}
             }
         }
     })
